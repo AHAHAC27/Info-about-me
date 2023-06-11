88dda6c0a2f8cac91179b1f0b47d326e733cf835
AHAHAC27 <134727630+AHAHAC27@users.noreply.github.com>
2023-06-11 16:59:16 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+int main()
+{
+	std::string myName = "AHAHAC27";
+	std::string myAdress= "https://github.com/AHAHAC27";
+	std::cout << "Hello! It is my name and my GitHub-adress" << std::endl;
+	std::cout << "My Name: " << myName << std::endl;
+	std::cout << "My adress: " << myAdress << std::endl;
+	system("pause");
+	return 0;
+}
